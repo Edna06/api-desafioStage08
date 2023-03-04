ae81253d65e18e41c96ecd35d2c77f83d763ee07
Edna <edna.maria887@gmail.com>
2023-03-04 14:19:04 -0300

    update info user

--- src/Controllers/UsersController.js
+++ src/Controllers/UsersController.js
@@ -20,9 +20,7 @@ class UsersController {
       throw new AppError('Usuário já está em uso!')
     }
 
-
     const encryptUserPassword = await hash(password, 8)
-
 
     //cadastrando novo usuário
     await database.run(
@@ -30,16 +28,36 @@ class UsersController {
       [name, email, encryptUserPassword]
     )
 
-
-
-
-
-
-
-
-
-
     return response.status(204).end()
+  }
+
+  async update(request, response) {
+    const { name, email } = request.body
+    const { id } = request.params
+
+    const database = await sqliteConnection()
+
+    const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+    //lançando erro caso o usuário não esteja cadastrado no banco de dados
+    if (!user) {
+      throw new AppError('Usuário não encontrado!')
+    }
+
+    const userWithUpdateEmail = await database.get( 'SELECT * FROM users WHERE email = (?)',[email])
+
+    //caso o email que esteja sendo inserido seja igual a de um usuário já cadastrado
+    if (userWithUpdateEmail && userWithUpdateEmail.id !== id) {
+      throw new AppError('Esse email já está em uso!')
+    }
+
+    //agora, atualizando o email do usuário
+    user.name = name
+    user.email = email
+
+  await database.run(` UPDATE users SET name = ?, email = ?, updated_at = ? WHERE id = (?)`, [user.name, user.email, new Date(), id])
+
+    return response.json()
   }
 }
 
