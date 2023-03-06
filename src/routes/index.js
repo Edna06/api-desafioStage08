817019752e1751ebbee835555d0651fbe239b4f1
Edna <edna.maria887@gmail.com>
2023-03-06 12:49:05 -0300

    project done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
 const {Router} = require('express')
-const notesRoutes = require('./notes.routes.js')
+const notesRouter = require('./notes.routes.js')
 const usersRouter = require('./users.routes.js')
+const tagsRouter = require('./tags.routes')
 
 const routes = Router()
 
 routes.use("/users", usersRouter)
-routes.use("/movie_notes", notesRoutes)
+routes.use("/movie_notes", notesRouter)
+routes.use("/movie_tags", tagsRouter)
+
 
 module.exports = routes
