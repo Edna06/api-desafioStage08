aa1f4902c3afd7ececd89fb03ea752862aa71f35
Edna <edna.maria887@gmail.com>
2023-04-20 23:55:51 -0300

    added Validating registered user

--- src/Controllers/SessionController.js
+++ src/Controllers/SessionController.js
@@ -1,9 +1,16 @@
-
+const knex = require('../database/knex')
+const AppError = require('../utils/AppError')
 class SessionController {
   async create(request,response){
   const {email, password} = request.body;
 
-  return response.json({email,password})
+  const user = await knex('users').where({email}).first()
+
+  if(!user) {
+    throw new AppError('E-mail e/ou senha incorreta', 401)
+  }
+
+  return response.json(user)
     }
 }
 
