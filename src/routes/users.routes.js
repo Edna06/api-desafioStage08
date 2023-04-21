5f04089293a445aeb41b477db8095eaaa7188d49
Edna <edna.maria887@gmail.com>
2023-04-21 18:41:46 -0300

    added saving avatar in database

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,22 +4,19 @@ const uploadConfig = require('../configs/upload')
 
 const UsersController = require('../Controllers/UsersController')
 const ensureAuthenticated = require('../Middleware/ensureAuthenticated')
+const UserAvatarController = require('../Controllers/UserAvatarController')
 
 const userRoutes = Router()
 
 const usersController = new UsersController()
+
+const userAvatarController = new UserAvatarController()
 
 const upload = multer(uploadConfig.MULTER) //o multer vai ser o responsável por carregar a nossa imagem
 
 userRoutes.post("/", usersController.create)
 userRoutes.put("/", ensureAuthenticated, usersController.update)
 
-userRoutes.patch("/avatar", ensureAuthenticated, upload.single('avatar'),
-(request, response) => {
-  console.log(request.file.filename)
-
-  response.json()
-  }
-)
+userRoutes.patch("/avatar", ensureAuthenticated, upload.single('avatar'), userAvatarController.update)
 
 module.exports = userRoutes
